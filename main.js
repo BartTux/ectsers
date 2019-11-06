9df49b5d5e80f7c99009c6c31a645dbe148d6edc
BartTux <bartpot98@gmail.com>
2019-11-06 08:02:01 +0100

    Start writing the Model array

---
+++ main.js
@@ -0,0 +1,15 @@
+class Model {
+    constructor() {
+        this.questions = [
+            { id: 1, question: 'Pytanie?', a: 'Ferryt', b: 'Perlit', c: 'Austenit', d: 'Ledeburyt', correct: questions[0].b, asked: false }
+        ];
+    }
+}
+
+class View {
+
+}
+
+class Controller {
+
+}
