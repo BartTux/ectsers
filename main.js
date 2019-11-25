ca2c2ba85c374d567654d39699543afec81b27f4
BartTux <bartpot98@gmail.com>
2019-11-25 08:03:35 +0100

    Start generating HTML via view

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ class Model {
             let drawnNumber = Math.floor(Math.random() * this._checkIfAsked().length);
 
             /*
-                Map function in this case create a new array contains all
+                Map function in this case creates a new array contains all
                 the questions id.
                 IndexOf function here returns the smallest array returned by map()
                 item index where this item value is equal to value given as indexOf()
@@ -61,7 +61,79 @@ class Model {
 }
 
 class View {
+    constructor() {
+        this.app = this.getElement('.container');
 
+        this.gameContent = this.createElement('div', 'game-content');
+
+        this.speakerImage = this.createElement('img', 'speaker');
+        this.speakerImage.src = 'images/speaker-body.png';
+        this.speakerImage.alt = 'Speaker';
+
+        this.ironCarbonSystemImage = this.createElement('img' ,'iron-carbon_system');
+        this.ironCarbonSystemImage.src = 'images/iron-carbon_system.png';
+        this.ironCarbonSystemImage.alt = 'Układ żelazo-węgiel';
+
+        this.questionForm = this.createElement('form', 'game-form');
+
+        this.questionLabel = this.createElement('label');
+        this.questionLabel.textContent = 'Niech Pan mi powie co to takiego jest ferryt?';
+
+        this.answerContainer = this.createElement('div', 'answer-container');
+
+        this.answerAButton = this.createElement('button', 'answer');
+        this.spanA = this.createElement('span', 'special-info');
+        this.spanA.textContent = 'A: ';
+
+        this.answerBButton = this.createElement('button', 'answer');
+        this.spanB = this.createElement('span', 'special-info');
+        this.spanB.textContent = 'B: ';
+
+        this.answerCButton = this.createElement('button', 'answer');
+        this.spanC = this.createElement('span', 'special-info');
+        this.spanC.textContent = 'C: ';
+
+        this.answerDButton = this.createElement('button', 'answer');
+        this.spanD = this.createElement('span', 'special-info');
+        this.spanD.textContent = 'D: ';
+
+        this.answerAButton.append(this.spanA);
+        this.answerAButton.textContent = 'Graniczny roztwór węgla w żelazie alfa';
+
+        this.answerBButton.append(this.spanB);
+        this.answerBButton.textContent = 'Węglik żelaza o wzorze sumarycznym Fe3C';
+
+        this.answerCButton.append(this.spanC);
+        this.answerCButton.textContent = 'Graniczny roztwór węgla w żelazie beta';
+
+        this.answerDButton.append(this.spanD);
+        this.answerDButton.textContent = 'Mieszanina eutektoidalna perlitu i cementytu';
+
+        this.answerContainer.append(this.answerAButton,
+            this.answerBButton, this.answerCButton, this.answerDButton);
+
+        this.questionForm.append(this.questionLabel, this.answerContainer);
+
+        this.gameContent.append(this.speakerImage, this.ironCarbonSystemImage, this.questionForm);
+
+
+    }
+
+    createElement(tag, className) {
+        const element = document.createElement(tag);
+        
+        if (className) {
+            element.classList.add(className);
+        } 
+        
+        return element;
+    }
+    
+    getElement(selector) {
+        const element = document.querySelector(selector);
+        
+        return element;
+    }
 }
 
 class Controller {
